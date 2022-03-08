bbfc2d0f71a39acee71dcecc04439ee2b1cab785
Zach Leatherman <zachleatherman@gmail.com>
2022-03-08 16:48:23 -0600

    Windows paths in tests

--- test/testServer.js
+++ test/testServer.js
@@ -1,12 +1,17 @@
 const test = require("ava");
-const EleventyDevServer = require("../")
+const path = require("path");
+const EleventyDevServer = require("../");
+
+function testNormalizeFilePath(filepath) {
+  return filepath.split("/").join(path.sep);
+}
 
 test("Url mappings for resource/index.html", t => {
   let server = EleventyDevServer.getServer("test-server", "./test/stubs/");
 
   t.deepEqual(server.mapUrlToFilePath("/route1/"), {
     statusCode: 200,
-    filepath: "test/stubs/route1/index.html"
+    filepath: testNormalizeFilePath("test/stubs/route1/index.html")
   });
 
   t.deepEqual(server.mapUrlToFilePath("/route1"), {
@@ -20,7 +25,7 @@ test("Url mappings for resource/index.html", t => {
 
   t.deepEqual(server.mapUrlToFilePath("/route1/index.html"), {
     statusCode: 200,
-    filepath: "test/stubs/route1/index.html"
+    filepath: testNormalizeFilePath("test/stubs/route1/index.html")
   });
 
   server.close();
@@ -40,12 +45,12 @@ test("Url mappings for resource.html", t => {
 
   t.deepEqual(server.mapUrlToFilePath("/route2"), {
     statusCode: 200,
-    filepath: "test/stubs/route2.html"
+    filepath: testNormalizeFilePath("test/stubs/route2.html")
   });
   
   t.deepEqual(server.mapUrlToFilePath("/route2.html"), {
     statusCode: 200,
-    filepath: "test/stubs/route2.html",
+    filepath: testNormalizeFilePath("test/stubs/route2.html",)
   });
 
   server.close();
@@ -57,22 +62,22 @@ test("Url mappings for resource.html and resource/index.html", t => {
   // Production mismatch warning: Netlify 301 redirects to /route3 here
   t.deepEqual(server.mapUrlToFilePath("/route3/"), {
     statusCode: 200,
-    filepath: "test/stubs/route3/index.html",
+    filepath: testNormalizeFilePath("test/stubs/route3/index.html",)
   });
 
   t.deepEqual(server.mapUrlToFilePath("/route3/index.html"), {
     statusCode: 200,
-    filepath: "test/stubs/route3/index.html",
+    filepath: testNormalizeFilePath("test/stubs/route3/index.html",)
   });
 
   t.deepEqual(server.mapUrlToFilePath("/route3"), {
     statusCode: 200,
-    filepath: "test/stubs/route3.html"
+    filepath: testNormalizeFilePath("test/stubs/route3.html")
   });
   
   t.deepEqual(server.mapUrlToFilePath("/route3.html"), {
     statusCode: 200,
-    filepath: "test/stubs/route3.html",
+    filepath: testNormalizeFilePath("test/stubs/route3.html",)
   });
 
   server.close();
